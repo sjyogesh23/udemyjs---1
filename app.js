01a11b31add86b6d1ab2e8e26e9df04169ea1448
Yogesh <sjyogesh23@gmail.com>
2023-01-21 23:30:58 +0530

    learning javascript with getelement. Till string in udemy

---
+++ app.js
@@ -0,0 +1,6 @@
+let number = 1;
+let resultnum = document.getElementById('num');
+resultnum.innerText = number;
+
+let randtxt = document.getElementsByClassName('randtxt')[number];
+randtxt.innerText = "heyy";
